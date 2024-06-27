24653542e5d4a6f4372c951f245e1573304dfaef
Joel Wetzell <jwetzell@yahoo.com>
2024-06-27 08:58:29 -0500

    fix test

--- test.js
+++ test.js
@@ -5,7 +5,12 @@ const decoder = new Decoder(10, 8);
 
 const rawData = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
 
-const encodedData = encoder.encode(rawData);
-const decodedData = decoder.decode(encodedData);
+
+const encodedData = rawData.map((sample)=>{
+    return encoder.encode(sample)
+})
+const decodedData = encodedData.map((sample)=>{
+    return decoder.decode(sample)
+});
 console.log(encodedData);
 console.log(decodedData);
